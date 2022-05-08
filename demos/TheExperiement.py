250ccba82f13d1bed6a6ce7402ebe81fcad54b46
kessler_lab kessler_lab <kessler_lab@auth.ad.bgu.ac.il>
2022-05-08 13:41:26 +0300

    5.8 4

--- demos/TheExperiement.py
+++ demos/TheExperiement.py
@@ -13,9 +13,9 @@ global events_df, vars_df, start_time
 global tracker
 
 # %%  Monitor/geometry
-subject = '207601006'
-COLORS = ['red', 'blue', 'red', 'blue', 'red', 'blue']
-BLOCKS = 5
+subject = '318657061'
+COLORS = ['blue', 'red', 'blue', 'red', 'blue', 'red', 'blue']
+BLOCKS = 4
 # NON_DOMINANT = 'red'
 # DOMINANT = 'blue'
 MY_MONITOR = 'testMonitor'  # needs to exists in PsychoPy monitor center
